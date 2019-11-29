6bf100956ba97332597b086b3ed552d2bb326486
JUN1113 <dunkelrots@gmail.com>
2019-11-29 20:38:58 +0900

    Update b.php

--- ABC/46/b.php
+++ ABC/46/b.php
@@ -1,5 +1,8 @@
 <?php
 // https://atcoder.jp/contests/abc046/tasks/abc046_b
+// N個のボールをK色で塗る、ただし隣り合う色は同一ではない
 
-// "a b c d e f"といった形で入力を受ける
-$array = explode(" ", trim(fgets(STDIN)));
+// 変数を$N,$Kとして入力する
+list($N, $K) = explode(" ", trim(fgets(STDIN)));
+
+echo ($K-1)**($N-1)*$K."\n";
